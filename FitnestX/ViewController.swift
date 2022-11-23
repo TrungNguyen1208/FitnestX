4dce6a5905ffec553fb2b575557b90cb3fb49a69
Evans Nguyen <thanhtrungnguyen1208@gmail.com>
2022-11-23 10:05:25 +0700

    Initial Commit

---
+++ FitnestX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnestX
+//
+//  Created by Trung Nguyen on 23/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
