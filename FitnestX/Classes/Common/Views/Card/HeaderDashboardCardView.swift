516747ec0aed992b0cca9a7dea04e351bd41d136
Tiến Ngô <ngoxuantien1999qvbn@gmail.com>
2023-06-18 21:18:08 +0700

    create banner card view, header card

---
+++ FitnestX/Classes/Common/Views/Card/HeaderDashboardCardView.swift
@@ -0,0 +1,19 @@
+//
+//  HeaderDashboardCardView.swift
+//  FitnestX
+//
+//  Created by Tiến Ngô on 18/06/2023.
+//
+
+import Foundation
+import UIKit
+
+final class HeaderDashboardCardView: NibView {
+    
+    
+    // MARK: LifeCycle
+    
+    override func configureView() {
+        super.configureView()
+    }
+}
