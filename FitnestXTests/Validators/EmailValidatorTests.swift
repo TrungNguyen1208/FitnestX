6728213344f7cc27f62a2c35970562cf91a6f32c
Evans Nguyen <evans.trungnguyen@setel.com>
2023-07-05 10:55:29 +0700

    Add unit test

---
+++ FitnestXTests/Validators/EmailValidatorTests.swift
@@ -0,0 +1,44 @@
+//
+//  EmailValidatorTests.swift
+//  FitnestXTests
+//
+//  Created by Trung Nguyen on 05/07/2023.
+//
+
+import XCTest
+@testable import FitnestX
+
+final class EmailValidatorTests: XCTestCase {
+  
+  var emailValidator: EmailValidator!
+  
+  override func setUp() {
+    super.setUp()
+    emailValidator = EmailValidatorImpl()
+  }
+  
+  override func tearDown() {
+    emailValidator = nil
+    super.tearDown()
+  }
+  
+  func testValidEmail() {
+    let result = emailValidator.validate("test@example.com")
+    XCTAssertEqual(result, .valid)
+  }
+  
+  func testEmptyEmail() {
+    let result = emailValidator.validate("")
+    XCTAssertEqual(result, .error(.invalid))
+  }
+  
+  func testInvalidEmail() {
+    let result = emailValidator.validate("invalid_email")
+    XCTAssertEqual(result, .error(.invalid))
+  }
+  
+  func testNilEmail() {
+    let result = emailValidator.validate(nil)
+    XCTAssertEqual(result, .error(.empty))
+  }
+}
